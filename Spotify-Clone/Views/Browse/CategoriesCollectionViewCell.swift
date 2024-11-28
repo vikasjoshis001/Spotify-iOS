2a5aeae9d299e2602849c502ee1f7845901383e6
Vikas <vikas@higgsbosonhealth.com>
2024-11-28 17:14:08 +0530

    Added API call for getCategories - getFeaturedPlaylists & getRecommendations API Calls stopped working due to spotify privacy hence removed

---
+++ Spotify-Clone/Views/Browse/CategoriesCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoriesCollectionViewCell.swift
+//  Spotify-Clone
+//
+//  Created by Vikas Joshi on 28/11/24.
+//
+
+import UIKit
+
+class CategoriesCollectionViewCell: UICollectionViewCell {
+    
+}
