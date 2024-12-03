c165c0793d8681715642000d67c4df9fb9dc2b58
Vikas <vikas@higgsbosonhealth.com>
2024-12-03 17:18:47 +0530

    Added styling for NavigationBar and TabBar

--- Spotify-Clone/Controllers/Core/TabBarViewController.swift
+++ Spotify-Clone/Controllers/Core/TabBarViewController.swift
@@ -11,6 +11,8 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupTabBar()
+
         // MARK: - Home screen setup
 
         let vc1 = HomeViewController()
@@ -46,5 +48,58 @@ class TabBarViewController: UITabBarController {
         // MARK: - Set view controllers to tab bar
 
         setViewControllers([nav1, nav2, nav3], animated: false)
+        setupNavBar(nav1: nav1,
+                    nav2: nav2,
+                    nav3: nav3)
+    }
+
+    private func setupTabBar() {
+        // Set the tab bar's background color
+        if #available(iOS 15.0, *) {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = UIColor.black // Background color
+            appearance.stackedLayoutAppearance.selected.iconColor = UIColor.systemBlue // Selected icon color
+            appearance.stackedLayoutAppearance.selected.titleTextAttributes = [
+                .foregroundColor: UIColor.systemBlue // Selected text color
+            ]
+            appearance.stackedLayoutAppearance.normal.iconColor = UIColor.systemGray // Unselected icon color
+            appearance.stackedLayoutAppearance.normal.titleTextAttributes = [
+                .foregroundColor: UIColor.systemGray // Unselected text color
+            ]
+            tabBar.standardAppearance = appearance
+            tabBar.scrollEdgeAppearance = appearance
+        } else {
+            tabBar.barTintColor = UIColor.systemGray5 // Background color for earlier versions
+            tabBar.tintColor = UIColor.systemBlue // Selected tab color
+            tabBar.unselectedItemTintColor = UIColor.systemGray // Unselected tab color
+        }
+    }
+
+    private func setupNavBar(nav1: UINavigationController,
+                             nav2: UINavigationController,
+                             nav3: UINavigationController)
+    {
+        let appearance = UINavigationBarAppearance()
+
+        // Configure appearance
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.black
+        appearance.titleTextAttributes = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.boldSystemFont(ofSize: 18)
+        ]
+        appearance.largeTitleTextAttributes = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.boldSystemFont(ofSize: 32)
+        ]
+
+        // Apply to all UINavigationControllers
+        [nav1, nav2, nav3].forEach { navController in
+            navController.navigationBar.standardAppearance = appearance
+            navController.navigationBar.scrollEdgeAppearance = appearance
+            navController.navigationBar.compactAppearance = appearance
+            navController.navigationBar.tintColor = UIColor.white
+        }
     }
 }
